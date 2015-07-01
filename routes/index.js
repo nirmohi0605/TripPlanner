ea3988ce94d464dfb6571c0776311fb538b869fd
Nirmohi <nirmohi0605@gmail.com>
2015-07-01 13:09:22 -0400

    code before lunch

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/');
+var Promise = require('bluebird');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var selectionArr = [];
-    models.Hotel.find().exec()
-        .then(function(results) {
-            selectionArr.push(results);
-            return models.Restaurant.find().exec()
-        })
-        .then(function(results) {
-            selectionArr.push(results);
-            return models.ThingToDo.find().exec()
-        })
-        .then(function(results) {
-            selectionArr.push(results);
-            res.json(selectionArr);
-        })
-        .then(null, function(err) {
+    Promise.all([models.Hotel.find().exec(), models.ThingToDo.find().exec(), models.Restaurant.find().exec()])
+        .catch(function(err) {
             console.log(err);
         })
+        .spread(function(hotel, ThingToDo, restaurant) {
+            //res.json(data);
+            console.log("all done");
+            console.log(hotel);
+            console.log(ThingToDo);
+            console.log(restaurant);
+        });
+    // .then(function(results) {
+    //     res.send(results[2]);
+    // })
     //res.render('index', { title: 'Express' });
 
 });
